8e6debd7be5a59c5405cac613699154a709d9ab3
lgw <756117890@qq.com>
2020-05-13 21:48:10 +0800

    first commit

--- src/main/java/com/catfilm/springboot/controller/check/CheckParams.java
+++
@@ -1,7 +0,0 @@
-package com.catfilm.springboot.controller.check;
-
-import com.catfilm.springboot.controller.exception.ParamsException;
-
-public interface CheckParams {
-    public void checkInput() throws ParamsException;
-}
